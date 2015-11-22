dc662ecba0f0a4b28c92b11ba4a7e4e98b11c06a
Nick Dubus <nico.dubus17@gmail.com>
2015-11-22 14:27:04 -0500

    Add fantasy simulator

--- Programming_stuff/angular/seg-4110-angular/app/js/controllers.js
+++ Programming_stuff/angular/seg-4110-angular/app/js/controllers.js
@@ -4,10 +4,40 @@
 
 var nflPlayerControllers = angular.module('nflPlayerControllers', []);
 
-nflPlayerControllers.controller('PlayerListCtrl', ['$scope', 'Player',
-  function($scope, Player) {
+nflPlayerControllers.controller('PlayerListCtrl', ['$scope', 'Player', '$interval',
+  function($scope, Player, $interval) {
     $scope.players = Player.query();
     $scope.orderProp = 'age';
+    $scope.score = 0;
+
+    var timer = null
+
+    $scope.startSimulation = function() {
+        timer = $interval(function() {
+          var p;
+          for (var i = 0; i < $scope.players.length; i++) {
+            p = $scope.players[i];
+            var bonus = 10 * Math.random();
+            var score = Math.random() * bonus;
+            p.score += score;
+            $scope.score += score;
+          }
+        }, 2000)
+    }
+
+    $scope.killSimulation = function() {
+      if(angular.isDefined(timer)) {
+        $interval.cancel(timer);
+        timer=undefined;
+      }
+    }
+
+    $scope.resetSimulation = function() {
+      for (var i = 0; i < $scope.players.length; i++) {
+        $scope.players[i].score = 0;
+        $scope.score = 0;
+      }
+    }
   }]);
 
 nflPlayerControllers.controller('PlayerDetailCtrl', ['$scope', '$routeParams', 'Player',
